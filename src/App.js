54afaa6bbaaa83278e069c8f7f73809a0171d765
eric <administrator@olweg.com>
2016-09-21 06:59:13 -0400

    updated app source. added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import Component from 'inferno-component';
 import logo from './inferno.png';
 import './App.css';
 
-const Heading = ({title}) => (
-  <div className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
+const Layout = ({app}) => (
+  <div className="container-fluid">
+  <div className="row">
+    <div className="col-sm-12">
+      <Header title="Inferno!"/>
+      <Content/>
+      <Footer/>
+    </div>
+  </div>
+  </div>
+)
+
+const Header = ({title}) => (
+  <div className="text-sm-center">
+    <img src={logo} id="logo" alt="logo" />
     <h2>Welcome to {title}</h2>
   </div>
 )
 
-class App extends Component {
+const Footer = () => (
+  <div className="text-sm-center text-muted">
+    Inferno starter 1.0.0-alpha11
+  </div>
+)
+
+class Content extends Component {
   constructor(props){
     super(props);
   }
@@ -21,14 +39,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Heading title="Inferno!"/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="alert alert-danger text-sm-center">
+        To get started, edit <code>src/App.js</code> and save to reload.
       </div>
     );
   }
 }
 
-export default App;
+
+export default Layout;
